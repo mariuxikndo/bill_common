d2afb256dfa16681c08a6a5f755b7a481ac91d46
eychamba <estyom.1@gmail.com>
2015-02-21 04:59:55 -0500

    1. Actualizacion: Agregamos funciones a libreria client.php

--- libraries/client.php
+++ libraries/client.php
@@ -41,5 +41,35 @@ class Client {
          $cupo_disponible = $cupo_asignado - $tot_ch_custodio - $tot_cxc;
          return number_decimal($cupo_disponible);
     }
+    
+    public function get_client_data($client_id) {
+        $join_cluase = array(
+            '0' => array('table'=>'billing_clientetipo ct','condition'=>'c.clientetipo_idclientetipo = ct.idclientetipo')
+        );
+        
+        $client_data = $this->ci->generic_model->get_join(
+                    'billing_cliente c', 
+                    array('c.PersonaComercio_cedulaRuc'=>$client_id),
+                    $join_cluase, 
+                    'c.PersonaComercio_cedulaRuc, c.nombres, c.apellidos, c.razonsocial razonsocial, c.direccion, c.diasCredito dias_credito, c.telefonos, c.celular, c.email, c.clientetipo_idclientetipo tipo_id, c.descuentomaxporcent desc_client, c.cupocredito cupo_credito, ct.descuento desc_ct', 
+                    1 
+                );
+        return $client_data;
+    }
+    
+    /*
+     *  Devuelve el maximo descuento permitido al cliente    
+     */
+    public function get_max_descuento($desc_client, $desc_tipo_client) { 
+         /* 
+          * establecemos el descuento maximo qu se puede aplicar al cliente
+          * se aplica el mayor, sea el individual o el descuento por tipo
+          */
+         $desc_max = $desc_client;
+         if($desc_tipo_client > $desc_max){
+             $desc_max = $desc_tipo_client;
+         }
+         return $desc_max;
+    }
                 
 }
