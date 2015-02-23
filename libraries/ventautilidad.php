5af0ee4b56d81e711c217f58a455ec1be160b248
eychamba <estyom.1@gmail.com>
2015-02-22 21:27:27 -0500

    1. Actualizacion: Actualizacion de metodos al calcular la utilidad. Estandarizados para se usados por transacciones de venta y ajustes de salida

--- libraries/ventautilidad.php
+++ libraries/ventautilidad.php
@@ -19,14 +19,22 @@ class Ventautilidad {
         $this->ci = & get_instance();
     }    
  
-    /* Registramos la utilidad generada por la venta */
-    public function save_utilidad_venta($venta_id, $costo, $pvp_bienes, $pvp_servicios,$tipotransaccion_cod) {
+    /* 
+     * Registramos la utilidad generada por la venta 
+     * $utilidad = '+' = suma la utlidad     
+     * $utilidad = '-' = resta la utlidad     
+     */
+    public function save_utilidad_venta($doc_id, $costo, $pvp_bienes, $pvp_servicios, $tipotransaccion_cod, $utilidad = '+') {
+        
+        echo '<br/>Bienes:'.$pvp_bienes. 'Servicios:'. $pvp_servicios.'<br/>';
+        
         $res = false;
-        if($pvp_bienes > 0){
+        /* No comparamos con $pvp_bienes porque este si podria venir en cero en caso de los ajustes de salida */
+        if($costo > 0){
             $utilidad = $pvp_bienes - $costo;
             $utilidad_porcent = ($utilidad * 100)/$costo;
             $data = array(
-                'venta_id' => $venta_id,
+                'venta_id' => $doc_id,
                 'costo' => $costo,
                 'pvp' => $pvp_bienes,
                 'utilidad' => $utilidad,
@@ -35,16 +43,24 @@ class Ventautilidad {
             );
             $res = $this->ci->generic_model->save($data, 'bill_venta_utilidad');            
         }
-        
+        echo 'FUERA'.$pvp_servicios.'******************';
+        /* La utililidad negativa que por lo general es por ajuste de salida, re registra como servicio en negativo */
         if($pvp_servicios > 0){
+            if($utilidad == '-'){
+                $pvp_servicios = $pvp_servicios * -1;
+                $utilidad_porcent = 0;
+            }
+        
+            echo 'Dentro'.$pvp_servicios.'AAAAAAAAAAAA';
             $data = array(
-                'venta_id' => $venta_id,
+                'venta_id' => $doc_id,
                 'costo' => 0,
                 'pvp' => $pvp_servicios,
                 'utilidad' => $pvp_servicios,
-                'utilidad_porcent' => 100,
+                'utilidad_porcent' => $utilidad_porcent,
                 'tipotransaccion_cod' => $tipotransaccion_cod,
             );
+            echo 'Guardando en bill_venta_utilidad';
             $res = $this->ci->generic_model->save($data, 'bill_venta_utilidad');            
         }
         
@@ -67,38 +83,34 @@ class Ventautilidad {
                     );
             
             return $total_utilidad;
+        }
+        
+        /* Se obtiene el costo total de la venta, restando ajustes de salida */
+        public function get_costo_total($venta_id, $tipotransaccion_cod = -1) {
+            $where_data = array('venta_id'=>$venta_id);
+            if($tipotransaccion_cod != -1){
+                $where_data['tipotransaccion_cod'] = $tipotransaccion_cod;
+            }
+            
+            $total_utilidad = $this->ci->generic_model->sum_table_field( 
+                        'bill_venta_utilidad', 
+                        'costo', 
+                        $where_data
+                    );
+            
+            return $total_utilidad;
         }    
     
         /*
          * Se presenta la utilidad en porcentaje, respecto al pvp y costo
          */
         public function get_utilidad_porcent($venta_id, $tipotransaccion_cod = -1) {
+            $total_utilidad = $this->get_utilidad_total($venta_id,$tipotransaccion_cod);            
+            $costo_total = $this->get_costo_total($venta_id,$tipotransaccion_cod);
             
-            $where_data = array('venta_id'=>$venta_id);            
-            if($tipotransaccion_cod != -1){
-                $where_data['tipotransaccion_cod'] = $tipotransaccion_cod;
-            }
+            $utilidad_porcent = ($total_utilidad * 100)/$costo_total;
             
-            $utilidad_porcent = $this->ci->generic_model->get_data( 
-                        'bill_venta_utilidad', 
-                        $where_data, 
-                        'utilidad_porcent',
-                        null, 
-                        $rows_num = 0
-                    );
-            
-            if($utilidad_porcent){
-                $sum_utilidad = 0;            
-                $cont = 0;
-                        foreach ($utilidad_porcent as $value) {
-                            $sum_utilidad += $value->utilidad_porcent;
-                            $cont++;
-                        }
-                $prom_utilidad = $sum_utilidad / $cont;            
-                return $prom_utilidad;                
-            }else{
-                return 'NN';
-            }
+            return $utilidad_porcent;            
         }    
 
 }
